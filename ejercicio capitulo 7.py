18aa91c2470c25575e1a5d343c3989430b45f343
cesarperez71199 <112841409+cesarperez71199@users.noreply.github.com>
2023-06-14 18:23:01 -0600

    Add files via upload

---
+++ ejercicio capitulo 7.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  6 19:49:17 2023
+
+@author: Cesar Perez
+"""
+
+nombre=[""]
+renglon= [5]
+columna=[10]
+tabla=[]
+
+for i in range(len(renglon)):
+    for k in range(columna):
+        nombre[0]=input("Dame el nombre: ")
+        
+    for j in range(columna):
+        edad=input("Dame la edad: ")
+        
+        if edad <=18 :
+            print("Morro")
+            
+        else:
+            print("Dato guardado")
+            
+    
+    for m in range(columna):
+        altura= int(input("Dame la altura en cm: "))
+        
+        if altura <=180 :
+            print("chaparro")
+            
+        else:
+            print("Dato guardado")
+            
+     
+    for n in range(columna):
+        peso= float(input("Dame el peso: "))
+        
+        if peso <=50 :
+            print("flaco")
+            
+        else:
+            print("Dato guardado")
+            
+    
+    for p in range(columna):
+        print("Posicion: P=1, D=2, M=3, D=4")
+        posicion=int(input("Dame la posicion con : "))
+        
+        if posicion !=2 :
+            print("Posicion no disponible")
+            
+        else:
+            print("Dato guardado")
+            
+            
+            
+                   
